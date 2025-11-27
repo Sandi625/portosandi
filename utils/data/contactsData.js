2bc338d45d0a8d7d04f52e38aa893c6f35d00e60
Sandi625 <110502593+Sandi625@users.noreply.github.com>
2025-11-27 15:55:17 +0700

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'sandipermadi625@gmail.com',
+    phone: '+6281330920809',
+    address: 'Jakarta, Indonesia',
+    github: 'https://github.com/Sandi625',
+    facebook: 'https://www.facebook.com/sandi.ap.509047?locale=id_ID',
+    linkedIn: 'https://www.linkedin.com/in/achmad-sandi-aji-permadi-842b83260/',
+    // twitter: 'https://twitter.com/said7388',
+    // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
---
+++ utils/data/educations.js
@@ -0,0 +1,14 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor of Applied Science (BASc) - Software Engineering Technology",
+    duration: "2021 - 2025",
+    institution: "State Polytechnic of Banyuwangi",
+  },
+  {
+    id: 2,
+    title: "Vocational High School",
+    duration: "2018 - 2021",
+    institution: "SMK PGRI 1 GIRI",
+  }
+]
